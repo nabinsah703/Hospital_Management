8aa7e7c097a9006e277a9256de55b4be241db1c3
Navin Sah <nabinsah703@gmail.com>
2025-02-01 12:24:15 +0545

    add identity and connection string for sql server.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using RepositoryClasses;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
